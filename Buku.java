f65a734173e35e55b843b6fbfd2079353b3f21d9
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-11 09:36:56 +0700

    edit file Buku.java

--- Buku.java
+++ Buku.java
@@ -2,4 +2,16 @@ public class Buku {
     String kodeBuku;
     String judul;
     int tahunTerbit;
+
+    Buku(String kodeBuku, String judul, int tahunTerbit) {
+        this.kodeBuku = kodeBuku;
+        this.judul = judul;
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    void tampilBuku() {
+        System.out.println("Kode Buku: " + kodeBuku);
+        System.out.println("Judul: " + judul);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
+    }
 }
