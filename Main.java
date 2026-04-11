8a43ba49e4114312dfcb31a4daa30747ecab7205
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-11 14:04:54 +0700

    edit file Main.java dan Peminjaman.java

--- Main.java
+++ Main.java
@@ -4,5 +4,12 @@ public class Main {
         Mahasiswa11 mahasiswa2 = new Mahasiswa11("67890", "Bob", "Sistem Informasi");
         mahasiswa1.tampilMahasiswa();
         mahasiswa2.tampilMahasiswa();
+
+
+        Buku[] buku = new Buku[2];
+        buku[0] = new Buku("B001", "Pemrograman Java", 2020);
+        buku[1] = new Buku("B002", "Struktur Data", 2019);
+        buku[0].tampilBuku();
+        buku[1].tampilBuku();
     }
 }
--- Peminjaman.java
+++ Peminjaman.java
@@ -1,3 +1,23 @@
 public class Peminjaman {
+    Mahasiswa11 mahasiswa;
+    Buku buku;
+
+    Peminjaman(Mahasiswa11 mahasiswa, Buku buku) {
+        this.mahasiswa = mahasiswa;
+        this.buku = buku;
+    }
+
+    void tampilPeminjaman() {
+        System.out.println("Peminjaman Buku:");
+        mahasiswa.tampilMahasiswa();
+        buku.tampilBuku();
+    }
     
+    int hitungDenda(int hariTerlambat) {
+        if (hariTerlambat > 0) {
+            return  (hariTerlambat * 500);
+        } else {
+            return 0;
+        }
+    }
 }
